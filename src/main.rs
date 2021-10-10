131d88536fb661e7a7ce6e2a52c69758981f57d9
tim <tmsteem82@gmail.com>
2021-10-10 11:30:05 +0200

    implement logic for registering and saving user data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use warp::{Filter, Rejection, Reply, http::Response};
+use warp::{Filter, Rejection, Reply, http::Response, http::StatusCode};
+use serde::{Deserialize};
+use std::{collections::HashMap, convert::Infallible, sync::Arc};
+use tokio::sync::{Mutex};
 
 type Result<T> = std::result::Result<T, Rejection>;
+type UsersDb = Arc<Mutex<HashMap<String, UserData>>>;
+
+#[derive(Debug, Clone, Deserialize)]
+struct UserData {
+    username: String,
+    password: String
+}
 
 #[tokio::main]
 async fn main() {
+    let users_db: UsersDb = Arc::new(Mutex::new(HashMap::new()));
+
     let default_route = warp::any().map(|| { "Welcome"});
     let register_path = warp::path("register");
     let register_routes = register_path
@@ -11,6 +23,8 @@ async fn main() {
     .map(|| "Please use a HTTP POST request to register")
     .or(register_path
         .and(warp::post())
+        .and(warp::body::json())
+        .and(with_users_db(users_db.clone()))
         .and_then(register_handler));
     
     let login_path = warp::path("login");
@@ -27,11 +41,21 @@ async fn main() {
 
 }
 
-async fn register_handler() -> Result<impl Reply> {
-    Ok(Response::builder().body("registered"))
+async fn register_handler(user: UserData, users_db: UsersDb) -> Result<impl Reply> {
+    println!("Received UserData: {:?}", user);
+    if users_db.lock().await.contains_key(&user.username) {
+        println!("User already exists");
+        return Ok(Response::builder().status(StatusCode::BAD_REQUEST).body("User already exists"));
+    }
+    users_db.lock().await.insert(user.username.clone(), user);
+    println!("Users database: {:?}", users_db);
+    Ok(Response::builder().status(StatusCode::CREATED).body("registered"))
 }
 
 async fn login_handler() -> Result<impl Reply> {
     Ok(Response::builder().body("logged in"))
 }
 
+fn with_users_db(users_db: UsersDb) -> impl Filter<Extract = (UsersDb,), Error = Infallible> + Clone {
+    warp::any().map(move || users_db.clone())
+}
